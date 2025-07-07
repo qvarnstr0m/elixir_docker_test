a3c41587f7a46dbd5ed1e1d4fb14e3fce69ca201
qvarnstr0m <martin.qvarnstrom@effectsoft.se>
2025-07-07 22:15:49 +0200

    Using env vars for port

--- lib/elixir_docker_test/endpoint.ex
+++ lib/elixir_docker_test/endpoint.ex
@@ -1,9 +1,11 @@
 defmodule ElixirDockerTest.Endpoint do
   def child_spec(_) do
+    port = String.to_integer(System.get_env("PORT") || "4000")
+
     Plug.Cowboy.child_spec(
       scheme: :http,
       plug: ElixirDockerTest.Router,
-      options: [port: 4000]
+      options: [ip: {0, 0, 0, 0}, port: port]
     )
   end
 end
